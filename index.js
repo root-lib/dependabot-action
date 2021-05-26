ac824b8042293ea01626a6518a63f7650fc2a2e2
root-lib <libin.babu@razorpay.com>
2021-05-26 18:32:54 +0530

    read file from previous step and set output.

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { promises: fs } = require('fs')
 
-try {
+const main = async () => {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  const nameToGreet = core.getInput('path');
+  console.log(`reading ${path}!....`);
+  let content = await fs.readFile(path, 'utf8')
+  core.setOutput("vulnAlerts", content);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+
 }
+main().catch(err => core.setFailed(err.message))
