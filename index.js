5ff89105de37747c5bd4ecf5d69644b2e8036e0c
root-lib <libin.babu@razorpay.com>
2021-05-27 00:46:26 +0530

    adding jira ticket part

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { promises: fs } = require('fs')
+const { promises: fs } = require('fs');
+jira_body={
+  "fields": {
+     "project":
+     {
+        "key": "SEC"
+     },
+     "summary": "security ticket sample, report @security if you see this",
+     "description": "well i was supposed to be replaced",
+     "issuetype": {
+        "name": "Bug"
+     }
+ }
+}
+jira_headers={
+  'Accept': 'application/json',
+  'Content-Type': 'application/json',
+  'Authorization': ''
+}
 
 const main = async () => {
 
   const path = core.getInput('path');
   console.log(`reading ${path}!....`);
+  jira_headers.Authorization='Basic '+core.getInput('token');
+  const url = core.getInput('url');
+  var config={
+    method: 'POST',
+    headers: jira_headers,
+    body:''
+  }
   let content = await fs.readFile(path, 'utf8');
   let content = JSON.parse(content);
   resultSet=content.data.repository.vulnerabilityAlerts;
@@ -15,16 +40,16 @@ const main = async () => {
     createdDate=element.createdDate;
     packageName=element.securityVulnerability.package.packageName;
     var days =  Math.floor((Date.parse(new Date())-Date.parse(createdDate)  ) / 86400000);
-    if(days===0) {
+    if(days===1) {
       console.log(`${packageName} created at ${createdDate}`);
+      jira_body.summary="dependabot-"+packageName+"-"+createdDate;
+      jira_body.description="Autocreated for dependabot alerts from github for"+packageName;
+      config.body=jira_body;
+      const rawResponse = await fetch(url,config);
+      const jsonResponse = await rrawResponse.json();
+      console.log(jsonResponse);
     }
   });
-  for (const key in resultSet.nodes) {
-    if (Object.hasOwnProperty.call(object, key)) {
-      const element = object[key];
-      
-    }
-  }
   core.setOutput("ticketLists", content);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
