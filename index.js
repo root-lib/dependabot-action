3ebeeb676937c6eb9ce8dabdc6f0f7aac41bc9a6
root-lib <libin.babu@razorpay.com>
2021-05-27 16:35:21 +0530

    jira mandatory fields

--- index.js
+++ index.js
@@ -5,12 +5,45 @@ jira_body={
   "fields": {
      "project":
      {
-        "key": "SEC"
+        "key": "SBB"
      },
      "summary": "security ticket sample, report @security if you see this",
-     "description": "well i was supposed to be replaced",
+     "Description": "well i was supposed to be replaced",
      "issuetype": {
-        "name": "Bug"
+        "name": "Defect"
+     },
+     "customfield_10132":{
+       "id":"10701"
+     },
+     "customfield_10133":{
+      "id":"10153"
+    },
+    "customfield_10133":{
+      "id":"10153"
+    },
+     
+     "customfield_10500":"sample text",
+     "customfield_10502":"sample value",
+     "customfield_10805":{
+      "id":"10750"
+    },
+    "customfield_10806":{
+      "id":"10715"
+    },
+    "customfield_10807":{
+      "id":"10720"
+    },
+    "customfield_10808":{
+      "id":"10746"
+    },
+    "customfield_10808":{
+      "id":"10743"
+    },
+     "customfield_10810":"sample text",
+     "customfield_10811":"do or die",
+     "customfield_10812":"upgrade",
+     "priority":{
+       "name": "P2"
      }
  }
 }
@@ -37,21 +70,21 @@ const main = async () => {
   totalAlerts=resultSet.totalCount;
   vulnList=resultSet.nodes;
   vulnList.forEach(async(element) => {
-    console.log(JSON.stringify(element));
-    createdDate=element.createdDate;
+
+    createdDate=element.createdAt;
     packageName=element.securityVulnerability.package.packageName;
     var days =  Math.floor((Date.parse(new Date())-Date.parse(createdDate)  ) / 86400000);
     if(days===1) {
       console.log(`${packageName} created at ${createdDate}`);
       jira_body.summary="dependabot-"+packageName+"-"+createdDate;
-      jira_body.description="Autocreated for dependabot alerts from github for"+packageName;
+      jira_body.description="Auto created for dependabot alerts from github for"+packageName;
       config.body=jira_body;
       const rawResponse = await fetch(url,config);
       const jsonResponse = await rawResponse.json();
       console.log(jsonResponse);
     }
   });
-  core.setOutput("ticketLists", content);
+  //core.setOutput("ticketLists", content);
   // Get the JSON webhook payload for the event that triggered the workflow
   //const payload = JSON.stringify(github.context.payload, undefined, 2)
   //console.log(`The event payload: ${payload}`);
